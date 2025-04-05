b121803363edd4f3f67f6fa6aa01b86d8d6168f1
meirzhan <amirzhanmeirzhan5@gmail.com>
2025-04-06 00:59:38 +0500

    some changes

--- app/src/main/java/com/example/aifood/ui/theme/OnboardingPage.kt
+++ app/src/main/java/com/example/aifood/ui/theme/OnboardingPage.kt
@@ -29,6 +29,9 @@ fun OnboardingScreen() {
             Text(
                 text = "We serve incomparable delicacies",
             )
+            Text(
+                text = "Meiirzhan"
+            )
         }
     }
 }
