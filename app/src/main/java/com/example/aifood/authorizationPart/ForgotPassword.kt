a475cf29843206f3cb7b7d851e7cc84fa9c2f87b
meirzhan <amirzhanmeirzhan5@gmail.com>
2025-04-07 10:50:43 +0500

    Register

--- app/src/main/java/com/example/aifood/authorizationPart/ForgotPassword.kt
+++ app/src/main/java/com/example/aifood/authorizationPart/ForgotPassword.kt
@@ -1,8 +1,23 @@
 package com.example.aifood.authorizationPart
 
+import android.R.attr.text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
+
+@Preview
 @Composable
 fun ForgotPasswordScreen() {
-
+    Column() {
+        Text(
+            text = "Forgot Password",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.SemiBold,
+            lineHeight = 40.sp
+        )
+    }
 }
